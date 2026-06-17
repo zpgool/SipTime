65f6529df00e6a63f48fd9d4dcd5cd3c497c981f
이제호 <je23ss@naver.com>
2026-06-18 07:58:51 +0900

    feat: 목표 섭취량 설정 기능 구현

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,17 +1,103 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:siptime/widgets/bottom_navigation_bar.dart';
+import 'package:provider/provider.dart';
+import '../providers/water_provider.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
 
   @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  @override
   Widget build(BuildContext context) {
+    final provider = context.watch<WaterProvider>();
     return Scaffold(
-      appBar: AppBar(centerTitle: true, title: const Text('설정')),
+      appBar: AppBar(title: const Text("설정"), centerTitle: true),
       body: Padding(
-        padding: EdgeInsets.all(20),
-        
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "목표 설정",
+              style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+            ),
+
+            const SizedBox(height: 20),
+
+            Container(
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(18),
+                border: Border.all(color: Colors.grey.shade300),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "하루 목표 섭취량",
+                    style: TextStyle(fontSize: 15, color: Colors.grey),
+                  ),
+
+                  const SizedBox(height: 20),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CircleAvatar(
+                        radius: 24,
+                        backgroundColor: const Color(0xfff3f3f3),
+                        child: IconButton(
+                          onPressed: () {
+                            context.read<WaterProvider>().decreaseGoal();
+                          },
+                          icon: const Icon(Icons.remove, color: Colors.black),
+                        ),
+                      ),
+
+                      RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                              text: "${provider.goal}",
+                              style: const TextStyle(
+                                fontSize: 42,
+                                fontWeight: FontWeight.bold,
+                                color: Color(0xff4A90E2),
+                              ),
+                            ),
+                            const TextSpan(
+                              text: " mL",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w600,
+                                color: Color(0xff4A90E2),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+
+                      CircleAvatar(
+                        radius: 24,
+                        backgroundColor: const Color(0xfff3f3f3),
+                        child: IconButton(
+                          onPressed: () {
+                            context.read<WaterProvider>().increaseGoal();
+                          },
+                          icon: const Icon(Icons.add, color: Colors.black),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
